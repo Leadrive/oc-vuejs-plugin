203397dacc9ff94b866ce87119e33027aeca64f7
eternavsky <eugene.t@progforce.com>
2016-03-11 15:37:00 +0300

    routing

--- Plugin.php
+++ Plugin.php
@@ -4,6 +4,17 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'terne.vuejs::lang.plugin.name',
+            'description' => 'terne.vuejs::lang.plugin.description',
+            'author'      => 'Eugene Ternavsky',
+            'icon'        => 'icon-code',
+            'homepage'    => 'https://github.com/ternavsky/oc-vuejs-plugin'
+        ];
+    }
+
     public function registerSettings()
     {
     }
@@ -15,9 +26,19 @@ class Plugin extends PluginBase
         ];
     }
 
+    public function registerMarkupTags()
+    {
+        return [
+            'filters' => [
+                'json' => 'json_encode',
+            ]
+        ];
+    }
+
     public function boot()
     {
         \Event::listen('cms.page.display', function($controller, $url, $page) {
+            //dd($controller);
             //check if page requested by ajax and no handler
             if (
                 \Request::ajax() &&
@@ -28,15 +49,23 @@ class Plugin extends PluginBase
                 $content = $controller->renderPage() ?: '<!-- No content -->';
                 $vueComponents = [];
                 foreach ($page->components as $component) {
-                    if (method_exists($component, 'vueComponents')) {
+                    if (
+                        property_exists($component, 'vueComponents') &&
+                        is_array($component->vueComponents)
+                    ) {
 
                         //dd($component->getPath());
-                        $vueComponents_chunk = $component->vueComponents();
-                        foreach ($vueComponents_chunk as $vcName => $vcOptions) {
-                            $vueComponents_chunk[$vcName]['code'] = file_get_contents($component->getPath()."/".$vcOptions['file']);
+                        $vueComponents_chunk = $component->vueComponents;
+                        foreach ($vueComponents_chunk as $tag => $vcName) {
+                            // full vue components options
+                            if (is_integer($tag)) {
+                                $tag = str_slug($vcName, "-");
+                            }
                         }
 
-                        $vueComponents = array_merge($vueComponents, $vueComponents_chunk);
+                        $vueComponents = array_merge($vueComponents, [
+                            $tag => $vcName
+                        ]);
                     }
                 }
 
